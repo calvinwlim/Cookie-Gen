702ff77058c4daf2eaa75fccfb633ac7eb28be0d
pengoof <54857860+calvinwlim@users.noreply.github.com>
2020-08-13 13:54:01 -0700

    Revert "v 1.0.2"
    This reverts commit 0a9514949643d62f7340dae7563894043f2665e3.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,8 @@
 from gen import options
-from gen import ftlGenerator, eastbayGenerator, champsGenerator, ftaGenerator
+from gen import generator
 import pyfiglet
 
 
 banner = pyfiglet.figlet_format('Cookie-Gen', font = "slant")
 print(banner)
-site = input('Select A Site: / Footlocker / Eastbay / Champs / Footaction   ')
-if site == 'Footlocker':
-    ftlGenerator()
-elif site == 'Eastbay':
-    eastbayGenerator()
-elif site == 'Champs':
-    champsGenerator()
-elif site == 'Footaction':
-    ftaGenerator()
-else:
-    print('Site Input Invalid')
+generator()
