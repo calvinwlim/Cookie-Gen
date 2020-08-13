963dca02596f71e35250fb0f500fc62036df7f38
pengoof <54857860+calvinwlim@users.noreply.github.com>
2020-08-13 12:28:21 -0700

    v 1.0.1
    - added saving cookies to cookies.txt
    - cut cookie sinto usable value format

--- src/gen.py
+++ src/gen.py
@@ -1,4 +1,5 @@
 import time
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -10,8 +11,10 @@ def generator():
     driver = webdriver.Chrome(options=options, executable_path='../chromedriver')
     driver.get('https://www.footlocker.com')
     while driver != webdriver.Chrome(options=options, executable_path='../chromedriver'):
+        savedCookies = open('cookies.txt', 'a') 
         cookie = driver.get_cookie('_abck')
-        print(cookie)
+        print(cookie['value'], file = savedCookies) 
+        print('Generating...')
         time.sleep(1)
         driver.delete_cookie('_abck')
         time.sleep(2)
