0a9514949643d62f7340dae7563894043f2665e3
pengoof <54857860+calvinwlim@users.noreply.github.com>
2020-08-13 13:50:16 -0700

    v 1.0.2
    - added support for Eastbay, Champs, Footaction
    - prints generated cookies in console as well as text file

--- src/gen.py
+++ src/gen.py
@@ -7,14 +7,60 @@ options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
 
-def generator():
+def ftlGenerator():
     driver = webdriver.Chrome(options=options, executable_path='../chromedriver')
     driver.get('https://www.footlocker.com')
     while driver != webdriver.Chrome(options=options, executable_path='../chromedriver'):
+        print('Generating...')
         savedCookies = open('cookies.txt', 'a') 
         cookie = driver.get_cookie('_abck')
         print(cookie['value'], file = savedCookies) 
+        print(cookie['value'])
+        time.sleep(1)
+        driver.delete_cookie('_abck')
+        time.sleep(2)
+        driver.refresh()
+        time.sleep(2)
+
+def eastbayGenerator():
+    driver = webdriver.Chrome(options=options, executable_path='../chromedriver')
+    driver.get('https://www.eastbay.com')
+    while driver != webdriver.Chrome(options=options, executable_path='../chromedriver'):
         print('Generating...')
+        savedCookies = open('cookies.txt', 'a') 
+        cookie = driver.get_cookie('_abck')
+        print(cookie['value'], file = savedCookies) 
+        print(cookie['value'])
+        time.sleep(1)
+        driver.delete_cookie('_abck')
+        time.sleep(2)
+        driver.refresh()
+        time.sleep(2)
+
+def champsGenerator():
+    driver = webdriver.Chrome(options=options, executable_path='../chromedriver')
+    driver.get('https://www.champs.com')
+    while driver != webdriver.Chrome(options=options, executable_path='../chromedriver'):
+        print('Generating...')
+        savedCookies = open('cookies.txt', 'a') 
+        cookie = driver.get_cookie('_abck')
+        print(cookie['value'], file = savedCookies) 
+        print(cookie['value'])
+        time.sleep(1)
+        driver.delete_cookie('_abck')
+        time.sleep(2)
+        driver.refresh()
+        time.sleep(2)
+
+def ftaGenerator():
+    driver = webdriver.Chrome(options=options, executable_path='../chromedriver')
+    driver.get('https://www.footaction.com')
+    while driver != webdriver.Chrome(options=options, executable_path='../chromedriver'):
+        print('Generating...')
+        savedCookies = open('cookies.txt', 'a') 
+        cookie = driver.get_cookie('_abck')
+        print(cookie['value'], file = savedCookies) 
+        print(cookie['value'])
         time.sleep(1)
         driver.delete_cookie('_abck')
         time.sleep(2)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from gen import options
-from gen import generator
+from gen import ftlGenerator, eastbayGenerator, champsGenerator, ftaGenerator
 import pyfiglet
 
 
 banner = pyfiglet.figlet_format('Cookie-Gen', font = "slant")
 print(banner)
-generator()
+site = input('Select A Site: / Footlocker / Eastbay / Champs / Footaction   ')
+if site == 'Footlocker':
+    ftlGenerator()
+elif site == 'Eastbay':
+    eastbayGenerator()
+elif site == 'Champs':
+    champsGenerator()
+elif site == 'Footaction':
+    ftaGenerator()
+else:
+    print('Site Input Invalid')
